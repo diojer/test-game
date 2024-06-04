34607f02e9965732beb85de78fb80384b873b707
diojer <diogodinosaurus@gmail.com>
2024-06-04 20:39:14 +0100

    Fixed relative paths for tilemap and tileset

---
+++ data/tilesets/mainlevbuild.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="mainlevbuild" tilewidth="16" tileheight="16" tilecount="2560" columns="64">
+ <image source="../sprites/tiles/mainlevbuild.png" width="1024" height="640"/>
+</tileset>
